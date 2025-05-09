18c77644625775243956ab9f64e750536cbc422f
hyunjun00 <parkhyunjun00@gmail.com>
2025-05-09 15:26:00 +0900

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by 박현준 on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by 박현준 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
