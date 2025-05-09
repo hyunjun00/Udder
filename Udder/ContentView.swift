6c6cc94d922559a287c38ba122e9f92a232d5ee3
hyunjun00 <parkhyunjun00@gmail.com>
2025-05-09 15:33:23 +0900

    Add label and images.

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Udder")
+                .font(.system(size: 40))
+                .padding(20)
+            Text("Fresh Milk, On Demand.")
         }
-        .padding()
+        Spacer()
     }
 }
 
